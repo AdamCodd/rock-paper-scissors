4aca2cff4ab1f30d2db65482c37b7c1413720734
Adam <49641859+AdamCodd@users.noreply.github.com>
2022-07-07 21:29:28 +0200

    Add logic of the game

---
+++ game.js
@@ -0,0 +1,24 @@
+let playChoice = ['Rock', 'Paper', 'Scissors'];
+let promptSelection = prompt('Rock, Paper or Scissors?');
+let playerSelection = promptSelection.charAt(0).toUpperCase() + promptSelection.slice(1).toLowerCase();
+let computerSelection = computerPlay();
+
+function computerPlay() {
+    let randomChoice = Math.floor(Math.random() * 3);
+    return playChoice[randomChoice];
+}
+
+function roundPlay(playerSelection, computerSelection) {
+    let resultPlay;
+    if (playerSelection === computerSelection) {
+        return resultPlay = "Draw! The computer used " + computerSelection + " too!";
+    }
+    else if ((playerSelection === "Rock" && computerSelection === "Paper") || (playerSelection === "Scissors" && computerSelection === "Rock") || (playerSelection === "Paper" && computerSelection === "Scissors")) {
+        return resultPlay = "You lose! " + computerSelection + " beats your " + playerSelection;
+    }
+    else {
+        return resultPlay = "You win! Your " + playerSelection + " beats " + computerSelection;
+    }
+}
+roundPlay(playerSelection, computerSelection);
+
