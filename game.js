5c72d923852db71a14ca4604bc5cbe5be2c08b05
Adam <49641859+AdamCodd@users.noreply.github.com>
2022-07-08 00:50:22 +0200

    Formatting and clean-up

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 let playChoice = ['Rock', 'Paper', 'Scissors'];
 let promptSelection = prompt('Rock, Paper or Scissors?');
 let playerSelection = promptSelection.charAt(0).toUpperCase() + promptSelection.slice(1).toLowerCase();
+
 let computerSelection = computerPlay();
 
 function computerPlay() {
@@ -8,17 +9,20 @@ function computerPlay() {
     return playChoice[randomChoice];
 }
 
-function roundPlay(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     let resultPlay;
     if (playerSelection === computerSelection) {
         return resultPlay = "Draw! The computer used " + computerSelection + " too!";
     }
-    else if ((playerSelection === "Rock" && computerSelection === "Paper") || (playerSelection === "Scissors" && computerSelection === "Rock") || (playerSelection === "Paper" && computerSelection === "Scissors")) {
+    else if ((playerSelection === "Rock" && computerSelection === "Paper") || 
+    (playerSelection === "Scissors" && computerSelection === "Rock") || 
+    (playerSelection === "Paper" && computerSelection === "Scissors")) 
+    {
         return resultPlay = "You lose! " + computerSelection + " beats your " + playerSelection;
     }
     else {
         return resultPlay = "You win! Your " + playerSelection + " beats " + computerSelection;
     }
 }
-roundPlay(playerSelection, computerSelection);
-
+// Play once
+playRound(playerSelection, computerSelection);
